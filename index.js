7272e9d45a201d73d676c17207e53e0bd87541a3
Nic Champagne Williamson <champnic@microsoft.com>
2024-05-28 23:47:29 -0700

    Switch back to PAT for now.

--- index.js
+++ index.js
@@ -150,19 +150,21 @@ async function connectToAdo() {
 	try {
 		let adoAuthHandler = null;
 
-		// Otherwise, assume that the Azure CLI has already authenticated using
-		// `az login`.
-		const credential = new AzureCliCredential();
-		// Scope can be AdoAppClientID, or "'api://<API_APPLICATION_ID>/.default'"
-		const accessToken = await credential.getToken("api://AzureADTokenExchange/.default");
-		if (accessToken.token) { console.log("Got ADO token"); }
-		adoAuthHandler = azdev.getBearerHandler(accessToken.token);
-
-		// TODO: Add fallback here to use PAT if available.
-		// Use Personal Access Token (PAT) for authentication if set
-		//if (process.env.ado_token) {
-		//	adoAuthHandler = azdev.getPersonalAccessTokenHandler(process.env.ado_token);
-		//}
+		if (false) {
+			// Otherwise, assume that the Azure CLI has already authenticated using
+			// `az login`.
+			const credential = new AzureCliCredential();
+			// Scope can be AdoAppClientID, or "'api://<API_APPLICATION_ID>/.default'"
+			const accessToken = await credential.getToken("api://AzureADTokenExchange/.default");
+			if (accessToken.token) { console.log("Got ADO token"); }
+			adoAuthHandler = azdev.getBearerHandler(accessToken.token);
+		} else {
+			// TODO: Add fallback here to use PAT if available.
+			// Use Personal Access Token (PAT) for authentication if set
+			if (process.env.ado_token) {
+				adoAuthHandler = azdev.getPersonalAccessTokenHandler(process.env.ado_token);
+			}
+		}
 
 		const orgUrl = "https://dev.azure.com/" + core.getInput('ado_organization');
 		const adoConnection = new azdev.WebApi(orgUrl, adoAuthHandler);
