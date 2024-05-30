d844be2fb88ffbdc6fca9bde0795232fdddc7de2
Nic Champagne Williamson <champnic@microsoft.com>
2024-05-29 17:15:02 -0700

    t

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function main() {
 				'Accept': 'application/json'
 			} 
 		});
+		console.log("Response: " + response);
 		const data = await response.json();
 		console.log("Data: " + data);
 
